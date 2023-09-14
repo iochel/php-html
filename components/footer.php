4a0ced54111dd509d76f326545cfa5bf3c3a1b39
iochel <chele.cocjin@gmail.com>
2023-09-14 16:37:40 +0800

    my-update

---
+++ components/footer.php
@@ -0,0 +1,23 @@
+<!--FOOTER Section-->
+
+<footer>
+
+    <div class="footerContent">
+        <div class="socialmedia">
+            <a href=""><i class="fa-brands fa-facebook"></i></a> 
+            <a href=""><i class="fa-brands fa-instagram"></i></a>
+            <a href=""><i class="fa-brands fa-twitter"></i></a> 
+            <a href=""><i class="fa-brands fa-google-plus"></i></a>
+            <a href=""><i class="fa-brands fa-youtube"></i></a>
+        </div>
+    </div>
+    <div class="NavFooter">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">News</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">Our Team</a></li>
+        </ul>
+    </div>
+</footer>
---
+++ components/header.php
@@ -0,0 +1,18 @@
+<!--Header/Navigation-->
+
+
+
+<nav>
+        <div class="logo">
+            <img src="image/" alt=""> <i></i>
+        </div>
+        <ul>
+            <li><a href="home.php">home</a></li>
+            <li><a href="about.php">about</a></li>
+            <li><a href="contacts.php">contacts</a></li>
+            <li><a href="#" class="btn"> log in</a> </li><!-- Added onclick event to open the login popup -->
+        </ul>
+    </nav> 
+    
+</nav>
+    
